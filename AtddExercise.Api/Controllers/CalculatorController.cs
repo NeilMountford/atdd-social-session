9ab6af3133dd230d75d9d94ba3e5d194e5bf60b9
Neil Mountford <neil.mountford@launchb.co.uk>
2020-02-04 16:38:29 +0000

    Make initial test pass

---
+++ AtddExercise.Api/Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtddExercise.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add/{numbers}")]
+        public IActionResult Add(string numbers)
+        {
+            return Ok(numbers.Split(",").Select(int.Parse).Sum());
+        }
+    }
+}
