95fa9ccc08f48fedac38aa56e82383dae9c03689
Neil Mountford <neil.mountford@launchb.co.uk>
2020-02-05 09:54:16 +0000

    Implement save logic

--- AtddExercise.Api/Controllers/CalculatorController.cs
+++ AtddExercise.Api/Controllers/CalculatorController.cs
@@ -7,10 +7,19 @@ namespace AtddExercise.Api.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        private readonly IDataAccess _dataAccess;
+
+        public CalculatorController(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+        
         [HttpGet("add/{numbers}")]
         public IActionResult Add(string numbers)
         {
-            return Ok(numbers.Split(",").Select(int.Parse).Sum());
+            var result = numbers.Split(",").Select(int.Parse).Sum();
+            _dataAccess.SaveInputsAndResult(numbers, result.ToString());
+            return Ok(result);
         }
         
         [HttpGet("add")]
